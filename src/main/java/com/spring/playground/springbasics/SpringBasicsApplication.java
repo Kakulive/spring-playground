6e55a5e9258fae16c95bb620a6c6125300e18bd3
Kaku <79337205+Kakulive@users.noreply.github.com>
2022-04-03 21:53:57 +0200

    Components and dependencies added to Binary Search algorithm

--- src/main/java/com/spring/playground/springbasics/SpringBasicsApplication.java
+++ src/main/java/com/spring/playground/springbasics/SpringBasicsApplication.java
@@ -1,20 +1,19 @@
 package com.spring.playground.springbasics;
 
 import com.spring.playground.springbasics.algorithms.BinarySearchImpl;
-import com.spring.playground.springbasics.algorithms.BubbleSortAlgorithm;
-import com.spring.playground.springbasics.algorithms.QuickSortAlgorithm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBasicsApplication {
 
-	public static void main(String[] args) {
-		final var binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
-		int result = binarySearch.binarySearch(new int[]{12, 4, 6}, 3);
-		System.out.println(result);
+    public static void main(String[] args) {
 
-		SpringApplication.run(SpringBasicsApplication.class, args);
-	}
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBasicsApplication.class, args);
+        BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+        int result = binarySearch.binarySearch(new int[]{12, 4, 6}, 3);
+        System.out.println(result);
+    }
 
 }
--- src/main/java/com/spring/playground/springbasics/algorithms/BubbleSortAlgorithm.java
+++ src/main/java/com/spring/playground/springbasics/algorithms/BubbleSortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.spring.playground.springbasics.algorithms;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BubbleSortAlgorithm implements SortAlgorithm {
 
     public int[] sort(int[] numbers) {
