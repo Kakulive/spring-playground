0d88ee81b329c8606de18878ef7589046eaf3a5d
Kaku <79337205+Kakulive@users.noreply.github.com>
2022-04-06 19:21:55 +0200

    project cleanup

--- src/main/java/com/spring/playground/springbasics/SpringBasicsApplication.java
+++ src/main/java/com/spring/playground/springbasics/SpringBasicsApplication.java
@@ -1,19 +1,11 @@
 package com.spring.playground.springbasics;
 
-import com.spring.playground.springbasics.algorithms.BinarySearchImpl;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBasicsApplication {
 
     public static void main(String[] args) {
-
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBasicsApplication.class, args);
-        BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
-        int result = binarySearch.binarySearch(new int[]{12, 4, 6}, 3);
-        System.out.println(result);
     }
 
 }
--- src/main/java/com/spring/playground/springbasics/algorithms/SortAlgorithm.java
+++
@@ -1,5 +0,0 @@
-package com.spring.playground.springbasics.algorithms;
-
-public interface SortAlgorithm {
-    public int[] sort(int[] numbers);
-}
