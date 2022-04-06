08de92a72ea134c424d14864922ff2c40c6ad38d
Kaku <79337205+Kakulive@users.noreply.github.com>
2022-04-06 19:35:34 +0200

    dependencies hotfixed

--- src/main/java/com/spring/playground/springbasics/SpringBasicsApplication.java
+++ src/main/java/com/spring/playground/springbasics/SpringBasicsApplication.java
@@ -1,11 +1,13 @@
 package com.spring.playground.springbasics;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBasicsApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(SpringBasicsApplication.class, args);
     }
 
 }
