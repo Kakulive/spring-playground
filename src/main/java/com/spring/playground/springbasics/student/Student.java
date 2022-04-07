8e3ba9bda70ad0c9c0780e5f54dbc411dc53dfc0
Kaku <79337205+Kakulive@users.noreply.github.com>
2022-04-07 22:56:57 +0200

    student entity added

--- src/main/java/com/spring/playground/springbasics/student/Student.java
+++ src/main/java/com/spring/playground/springbasics/student/Student.java
@@ -1,7 +1,9 @@
 package com.spring.playground.springbasics.student;
 
 import lombok.*;
+import org.hibernate.annotations.Table;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Getter
@@ -9,7 +11,19 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
