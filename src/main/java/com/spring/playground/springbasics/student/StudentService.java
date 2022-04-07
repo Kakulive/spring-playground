78748e7df97d2cc5f466aa0da7b97b8e2b6049a1
Kaku <79337205+Kakulive@users.noreply.github.com>
2022-04-07 22:36:58 +0200

    Student Controller and Service implemented

---
+++ src/main/java/com/spring/playground/springbasics/student/StudentService.java
@@ -0,0 +1,20 @@
+package com.spring.playground.springbasics.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(new Student(
+                1L,
+                "Janusz",
+                "janusz.kowalski@gmail.com",
+                LocalDate.of(2000, Month.JANUARY, 5),
+                21
+        ));
+    }
+}
